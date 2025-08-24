4bbce80f0ff10d282ff0d17e6ed5e6d6d928a2ef
Renato Silva <araujo.renato@unemat.br>
2025-08-24 19:32:34 -0400

    push

--- Ferramenta_para_Importacao/pandas_implementacao.py
+++ Ferramenta_para_Importacao/pandas_implementacao.py
@@ -1,8 +1,8 @@
 import pandas as pd
+from verificar_PATH import VerificarPATH
 
 ## Criar uma classe para importar os dados e criar uma lista para ler todos os arquivo em uma array de dados
 PATH = r"F:\Introducao a Eng de Dados\Importacao dados aberto Receita Federal\Importacao dos dados\Dados_Brutos\Cnaes\F.K03200$Z.D50809.CNAECSV"
-
 
 class Pandas:
     def __init__(self, path, nome):
@@ -10,15 +10,16 @@ class Pandas:
         self.nome = nome
         self.df = None                                                        # Inicializa o atributo dentro da classe
 
+
     # Cria um Data Frame do dados
-    def criando_dataframe(self):
+    def criar_dataframe(self):
         self.df = pd.read_csv(
             self.path,                                                        #Dados de CNAE
             sep=';',                                                          #Separador utilizado no arquivo
             encoding='latin1',                                                #Encoder dos dados
             dtype=str                                                         #Formatar dados como String
         )
-        return self.df                                                      #Retorna o DataFrame
+        return self.df                                                        #Retorna o DataFrame
 
     def head(self, n=5):
         if self.df is not None:
@@ -28,7 +29,7 @@ class Pandas:
 
 meu_objeto = Pandas(PATH, "CNAE")
 
-meu_objeto.criando_dataframe()
+meu_objeto.criar_dataframe()
 
 print(meu_objeto.head())
 
