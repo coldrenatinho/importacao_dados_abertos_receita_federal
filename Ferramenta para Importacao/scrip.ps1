038fb90f25631b2771b852799d0665f5c7c3c43f
Renato Silva <araujo.renato@unemat.br>
2025-08-24 01:19:05 -0400

    Adicionar arquivos de configuração e scripts iniciais para o projeto

---
+++ Ferramenta para Importacao/scrip.ps1
@@ -0,0 +1,12 @@
+# Cria um ambiente virtual
+python -m venv venv
+
+# Entra no ambiente virtual
+.\venv\Scripts\activate
+
+# Instala os requerimentos
+pip install -r requirements.txt
+
+########### GERAR ARQUIVO DE REQUERIMENTO ###################
+# pip freeze > requirements.txt
+
