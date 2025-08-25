21a01c3b3919c64362d8429e18f40aa431b80a45
Renato Silva <araujo.renato@unemat.br>
2025-08-24 20:34:52 -0400

    1.0

--- Ferramenta_para_Importacao/main.py
+++ Ferramenta_para_Importacao/main.py
@@ -1,12 +1,29 @@
-# This is a sample Python script.
-
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
-
+from conexao_sql_server import ConexaoSQLServer
+from pandas_implementacao import Pandas
+from importador_sql import ImportadorSQL
 
 def main():
-    
+
+    criar_conexao = ConexaoSQLServer(
+        server="localhost",
+        database="Importacao_Dados_Receita_Federal",
+        username="federal",
+        password="SenhaForte123!"
+    )
+    criar_conexao.test_connection()
+
+    engine = criar_conexao.get_enige()
+
+    PATH = r"F:\Introducao a Eng de Dados\Importacao dados aberto Receita Federal\Importacao dos dados\Dados_Brutos\Cnaes\F.K03200$Z.D50809.CNAECSV"
+    meu_objeto = Pandas(PATH, "CNAE")
+    df = meu_objeto.criar_dataframe()
+
+    importador = ImportadorSQL(engine, df)
+
+    print(importador.ver_head())
+
+    importador.inserir_tabela("CNAE")
 
 if __name__ == '__main__':
+    main()
 
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
