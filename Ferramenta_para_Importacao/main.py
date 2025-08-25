02a14af929a6fcf75de3b4ad7fe5446655306264
Renato Silva <araujo.renato@unemat.br>
2025-08-24 23:05:29 -0400

    Versão 3.0

--- Ferramenta_para_Importacao/main.py
+++ Ferramenta_para_Importacao/main.py
@@ -1,11 +1,23 @@
+from concurrent.futures import ThreadPoolExecutor, as_completed
 from conexao_sql_server import ConexaoSQLServer
 from pandas_implementacao import Pandas
 from importador_sql import ImportadorSQL
 from listar_arquivos import ListarArquivos
-from enums import TipoArquivo
+
+MAX_TREADS = 5
+def processar_arquivo(path, tipo, engine):
+    """Função que lê o arquivo e insere no SQL"""
+    try:
+        meu_objeto = Pandas(path, tipo.name)
+        df = meu_objeto.criar_dataframe()
+        importador = ImportadorSQL(engine, df)
+        importador.inserir_tabela(tipo.name)
+        return f"{path} importado com sucesso ({len(df)} linhas)"
+    except Exception as e:
+        return f"Erro ao importar {path}: {e}"
 
 def main():
-    # 1. Conexão com SQL Server
+    # Conexão SQL Server
     criar_conexao = ConexaoSQLServer(
         server="localhost",
         database="Importacao_Dados_Receita_Federal",
@@ -15,30 +27,25 @@ def main():
     criar_conexao.test_connection()
     engine = criar_conexao.get_enige()
 
-    # 2. Listar todos os arquivos
+    # Listar arquivos
     path_dados = r"F:\Introducao a Eng de Dados\Importacao dados aberto Receita Federal\Importacao dos dados\Dados_Brutos"
     lista_arquivos = ListarArquivos(path_dados)
     resultados = lista_arquivos.listar_arquivos()
 
-    # 3. Iterar sobre os tipos de arquivos
+    # Preparar todos os jobs
+    jobs = []
     for arquivo in resultados:
-        if not arquivo.paths:  # pula se não houver arquivos
-            continue
-
-        print(f"\nImportando {arquivo.tipo.name} ({len(arquivo.paths)} arquivos)...")
-
         for path in arquivo.paths:
-            # Cria DataFrame
-            meu_objeto = Pandas(path, arquivo.tipo.name)
-            df = meu_objeto.criar_dataframe()
+            jobs.append((path, arquivo.tipo, engine))
 
-            # Insere no SQL Server
-            importador = ImportadorSQL(engine, df)
-            importador.inserir_tabela(arquivo.tipo.name)
+    # Executar em Threads
+    max_threads= MAX_TREADS
+    with ThreadPoolExecutor(max_workers=max_threads) as executor:
+        future_to_path = {executor.submit(processar_arquivo, path, tipo, engine): path for path, tipo, engine in jobs}
 
-            print(f"  - {path} importado ({len(df)} linhas)")
+        for future in as_completed(future_to_path):
+            resultado = future.result()
+            print(resultado)
 
 if __name__ == '__main__':
     main()
-
-
